774a93b07ada291d61a76c64127fca2bc1ac849e
Jeferson Lima <jefersonsilvadelima15@gmail.com>
2022-05-31 21:37:56 -0300

    Iniciando SOLID

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,24 @@
-import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
+import { request, response, Router } from 'express';
+import { CreateCategoryService } from '../services/CreateCategoryService';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidV4(),
-    name,
-    description
-  };
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  categories.push({category});
+  createCategoryService.execute({ name, description});
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
